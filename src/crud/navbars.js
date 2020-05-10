c23e26b2909ef3b591885b7d7d96550afa592319
vinod <vinoth.it@gmail.com>
2020-05-10 19:33:21 +0530

    added navbar

--- src/crud/navbars.js
+++ src/crud/navbars.js
@@ -1,21 +1,36 @@
 import React from 'react'
 import { Link, withRouter } from 'react-router-dom'
+import {searchEmployeeDetails} from './userFuction'
 
 class Navbars extends React.Component {
+
+    constructor(props){
+        super(props)
+        
+        this.state = {
+            searchName : ''
+        }
+        this.onChange = this.onChange.bind(this);
+    }
+
+    onChange(e){
+        let userName = e.target.value;
+        searchEmployeeDetails(userName)
+    }
     render() {
-        const regLink =  (
-            <ul className = "navbar-nav">
-                <li className = "nav-item">
-                    <Link to = "/login" className="nav-link">Login</Link>
-                </li>
-                <li className = "nav-item">
-                    <Link to = "/register" className="nav-link">Register</Link>
-                </li>
-                <li className = "nav-item">
-                    <Link to = "/employee" className="nav-link">Employee</Link>
-                </li>
-            </ul>
-        )
+        // const regLink =  (
+        //     <ul className = "navbar-nav">
+        //         <li className = "nav-item">
+        //             <Link to = "/login" className="nav-link">Login</Link>
+        //         </li>
+        //         <li className = "nav-item">
+        //             <Link to = "/register" className="nav-link">Register</Link>
+        //         </li>
+        //         <li className = "nav-item">
+        //             <Link to = "/employee" className="nav-link">Employee</Link>
+        //         </li>
+        //     </ul>
+        // )
 
         // const userLink =  (
         //     <ul className = "navbar-nav">
@@ -29,23 +44,30 @@ class Navbars extends React.Component {
         // )
 
         return(
-            <nav className = "navbar navbar-expand-lg navbar-dark bg-dark rounded">
-                <button className = "navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbar1"
-                aria-controls="navbar1"
-                aria-expanded="false"
-                aria-label="Toggle navigation">
-                    <span className = "navbar-toggler-icon"></span>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse justify-content-md-center" id="navbar1">
-                    <ul className="navbar-nav">
-                        <li className = "nav-item">
-                            <Link to="/" className="nav-link">Home</Link>
-                        </li>
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li class="nav-item active">
+                        <Link to="/home" className="nav-link">Home</Link>
+                    </li>
+                    <li class="nav-item">
+                        <Link to="/login" className="nav-link">Login</Link>
+                    </li>
+                    <li class="nav-item">
+                        <Link to="/register" className="nav-link">Register</Link>
+                    </li>
+                    <li class="nav-item">
+                        <Link to="/employee" className="nav-link">Employee</Link>
+                    </li>
                     </ul>
-                    {regLink}
+                    <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" value = {this.state.searchName} onChange = {this.onChange} />
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                    {/* {regLink} */}
                 </div>
             </nav>
         )
